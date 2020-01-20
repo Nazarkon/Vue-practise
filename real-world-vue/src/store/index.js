9c059897e19b6d9160e078a168c215a128cbe029
Nazarkon <nazarmontsibovych@gmail.com>
2020-01-20 21:22:40 +0200

    Last changes

--- real-world-vue/src/store/index.js
+++ real-world-vue/src/store/index.js
@@ -4,8 +4,22 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user:{ id: 'abc123', name: 'Adam'},
+    categories: ['sustainbility',
+      'education',
+      'food',
+      'community',
+      'nature',
+      'housing'
+    ]
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  getters:{
+    catLength: state => {
+      return state.categories.length
+    }
+  }
 });
