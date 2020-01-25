fa2fd8546f393b21bd2d12bb264632764fa5e553
Nazarkon <nazarmontsibovych@gmail.com>
2020-01-26 00:17:21 +0200

    Notification Done

--- real-world-vue/src/store/store.js
+++ real-world-vue/src/store/store.js
@@ -3,13 +3,15 @@ import Vuex from "vuex";
 
 import * as user from "@/store/module/user.js";
 import * as event from "@/store/module/event.js";
+import * as notification from "@/store/module/notification.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     user,
-    event
+    event,
+    notification
   },
   state: {
     categories: [
